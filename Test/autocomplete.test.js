69463b3e91be6789e1e58000b974f032c2a03d75
Connor Bush <connormbush12@gmail.com>
2020-11-08 15:45:12 -0600

    beforeEach and test for dropdown to open

--- Test/autocomplete.test.js
+++ Test/autocomplete.test.js
@@ -1,4 +1,8 @@
-it('Testing autocomplete', () => {
+//Whenever we run tests, it can help to start from scratch so that the ordering of the tests don't interfere with whether they pass or not
+//To do this, we can use beforeEach, which is a method built into Mocha that runs a set of code before each test. It helps set up preconditions and clean up after tests.
+beforeEach(() => {
+    //Within beforeEach, we want to clear the target div's HTML and then run our Autocomplete function to reset the stage
+    document.querySelector('#target').innerHTML = ''
     createAutoComplete({
         root : document.querySelector('#target'),
         fetchData() {
@@ -12,9 +16,24 @@ it('Testing autocomplete', () => {
             return movie.Title;
         }
     })
-    //Next, we want to run some testing code for our dropdown menu. The first thing we want to check is to make sure the dropdown isn't open before we search anything.
-    //We use Chai Assertion Library for this. Mocha's browser support HTML already scripts in Chai. We also have to declare the expect variable in our HTML file to use it
+})
+
+//Now, we change the name of this test to make it more applicable to this test
+it('Dropdown starts closed', () => {
     const dropdown = document.querySelector('.dropdown')
-    //Once we complete a search, the class 'is-active' gets added to our dropdown div. Therefore, we want to make sure our dropdown div does not contain is-active when we first open the file 
     expect(dropdown.className).not.to.include('is-active')
+}) 
+
+//We also want to test that the dropdown opens up after we search
+it('Dropdown opens after search', () => {
+    //First, we select our input element
+    const input = document.querySelector('input')
+    //We give it a value (basically it's like we fake searched)
+    input.value = 'avengers'
+    //However, simply giving our input a value does not trigger an input event, which is what our code is written to listen for to trigger our fetchData function. Therefore, we can use .dispatchEvent in order to trigger a fake event to see if the dropdown opens up
+    input.dispatchEvent(new Event('input'))
+    
+    //Finally, we check to see if the dropdown includes the is-active class
+    const dropdown = document.querySelector('.dropdown')
+    expect(dropdown.className).to.include('is-active')
 }) 
