d8d16a5f7c35bd9fed387abd7817c5ce7095de2c
Connor Bush <connormbush12@gmail.com>
2020-11-08 15:55:11 -0600

    waitFor function to handle debounce

--- Test/autocomplete.test.js
+++ Test/autocomplete.test.js
@@ -1,7 +1,28 @@
-//Whenever we run tests, it can help to start from scratch so that the ordering of the tests don't interfere with whether they pass or not
-//To do this, we can use beforeEach, which is a method built into Mocha that runs a set of code before each test. It helps set up preconditions and clean up after tests.
+//In our movie-fight app, we have a debounce on our search item. Therefore, we have to create a function that waits for a certain action to happen before we progress with our code.
+//We do this with a promised-based function. It needs to be promised-based so that we can await the promise to be resolved before progressing to our new code OR so we can reject the promise and throw an error if what we need to occur never happens
+//We pass through the selector aka the item that we need to see in our DOM before we move on
+const waitFor = (selector) => {
+    //We create a new promise
+   return new Promise((resolve, reject) => {
+       //First, we set up an interval. The interval checks if that new item has appeared frequently. We chose 30 ms - you want it frequently enough so that it catches it quickly but not so frequently that it unneccesarily uses up a lot of our CPU
+       const interval = setInterval(() => {
+           if(document.querySelector(selector)) {
+               //If we do find it, we want to kill our timeout and interval and resolve the promise
+                clearInterval(interval)
+                clearTimeout(timeout)
+                resolve();
+           }
+       }, 30)
+
+       //If we are waiting on something that never occurs, we want to have a way of exiting the test and throwing an error. Therefore, we create a timeout and set it for about 2 seconds. If the thing we are looking for with our interval has not occurred after 2 seconds, it probably won't occur at all, so we clear the interval and reject the promise.
+       const timeout = setTimeout(() => {
+            clearInterval(interval)
+            reject();
+       }, 2000)
+   })
+}
+
 beforeEach(() => {
-    //Within beforeEach, we want to clear the target div's HTML and then run our Autocomplete function to reset the stage
     document.querySelector('#target').innerHTML = ''
     createAutoComplete({
         root : document.querySelector('#target'),
@@ -18,22 +39,19 @@ beforeEach(() => {
     })
 })
 
-//Now, we change the name of this test to make it more applicable to this test
 it('Dropdown starts closed', () => {
     const dropdown = document.querySelector('.dropdown')
     expect(dropdown.className).not.to.include('is-active')
 }) 
 
-//We also want to test that the dropdown opens up after we search
-it('Dropdown opens after search', () => {
-    //First, we select our input element
+it('Dropdown opens after search', async () => {
     const input = document.querySelector('input')
-    //We give it a value (basically it's like we fake searched)
     input.value = 'avengers'
-    //However, simply giving our input a value does not trigger an input event, which is what our code is written to listen for to trigger our fetchData function. Therefore, we can use .dispatchEvent in order to trigger a fake event to see if the dropdown opens up
     input.dispatchEvent(new Event('input'))
     
-    //Finally, we check to see if the dropdown includes the is-active class
+    //In this particular case, we are waiting for the dropdown items to appear. This signals that the dropdown menu has opened. Therefore, we don't check to see if the dropdown has opened until we see a dropdown item via our waitFor function. This is a promise-based function, so we want to use async above and await this
+    await waitFor('.dropdown-item');
+
     const dropdown = document.querySelector('.dropdown')
     expect(dropdown.className).to.include('is-active')
 }) 
